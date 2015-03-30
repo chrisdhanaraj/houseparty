3875e297857baef596e92caf27273b07d58fe5e1
Chris Dhanaraj <chris.dhanaraj@thomsonreuters.com>
2015-03-30 00:28:16 -0400

    abstracting pie chart out, needed to add d3 in

--- src/app/player/player-controller.js
+++ src/app/player/player-controller.js
@@ -9,9 +9,18 @@ angular.module('houseparty')
       PlayerModel.getMatchHistory(ctrl.id)
         .then(PlayerModel.getChampionData)
         .then(function(result) {
-          console.log(result);
           ctrl.champions = result;
-        })
+
+	  ctrl.createChampionChart();
+	});
+    };
+
+    ctrl.createChampionChart = function() {
+      ctrl.championTendency = {};
+      for (var i = 0; i < ctrl.champions.length; i++) {
+	var current = ctrl.champions[i];
+	ctrl.championTendency[current] = ctrl.championTendency[current] ? ctrl.championTendency[current] + 1 : 1;
+      }
     };
 
     ctrl.getHistory();
