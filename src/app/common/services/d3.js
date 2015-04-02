f0086e05999253d29b0ee056e64658a884eb78cf
Chris Dhanaraj <chris.dhanaraj@thomsonreuters.com>
2015-04-01 22:19:06 -0400

    abstracting out some services

---
+++ src/app/common/services/d3.js
@@ -0,0 +1,13 @@
+angular.module('houseparty.common.d3', [])
+  .factory('D3', function() {
+    var d3 = this;
+
+    var createPieChart = function(data) {
+
+    };
+
+    return {
+      createPieChart: createPieChart
+    }
+
+  });
---
+++ src/app/common/services/riot.js
@@ -0,0 +1,18 @@
+angular.module('houseparty.common.riot', [])
+  .service('Riot', function() {
+    var riot = this;
+
+    riot.extractData = function(data) {
+      return result.data.data;
+    };
+
+    riot.convertIdToChampion = function(id) {
+
+    };
+
+    riot.convertIdToName = function(id) {
+
+    };
+
+
+  });
