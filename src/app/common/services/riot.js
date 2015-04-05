9667e04293d844cddab879467668f1d08c3f00fc
Chris Dhanaraj <adhanaraj@gmail.com>
2015-04-05 10:34:41 -0400

    move riot api calls to here

--- src/app/common/services/riot.js
+++ src/app/common/services/riot.js
@@ -1,5 +1,5 @@
 angular.module('houseparty.common.riot', [])
-  .service('Riot', function(config) {
+  .service('Riot', function(config, $http) {
     var riot = this;
 
     // {{ID}}/entry?api_key=config.api
@@ -8,13 +8,26 @@ angular.module('houseparty.common.riot', [])
       'matchhistory' : 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/',
       'championData' : '/champions'
     };
-    var championData;
 
-    var getMatchHistoryUrl = function(id) {
-      return endpoints.matchhistory + id  + '?rankedQueues=RANKED_SOLO_5x5,RANKED_TEAM_5x5&api_key=' + config.api;
+    function extract(result) {
+      return result.data;
+    }
+
+    var getMatchHistoryUrl = function(id, number) {
+      return endpoints.matchhistory + id  + '?rankedQueues=RANKED_SOLO_5x5,RANKED_TEAM_5x5&endIndex=' + number + '&api_key=' + config.api;
     };
 
     var getLeagueUrl = function(id) {
       return endpoints.league + id + '/entry?api_key=' + config.api;
     };
+
+    riot.getLeague = function(id) {
+      return $http.get(getLeagueUrl(id)).then(extract);
+    };
+
+    riot.getMatchHistory = function(id, number) {
+      return $http.get(getMatchHistoryUrl(id, number)).then(extract);
+    };
+
+
   });
