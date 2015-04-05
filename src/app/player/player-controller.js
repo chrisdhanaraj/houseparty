7326a2da64c5fcd80e09009141cda1f2836f943c
Chris Dhanaraj <adhanaraj@gmail.com>
2015-04-05 18:30:40 -0400

    remove old ajax, add in modules

--- src/app/player/player-controller.js
+++ src/app/player/player-controller.js
@@ -1,14 +1,15 @@
 angular.module('player', [
   'houseparty.common.player',
   'houseparty.common.team',
+  'houseparty.common.champion',
   'player.championtendency',
   'player.killstats',
   'player.rolediversity',
   'player.stats10',
   'player.visiondata',
   'player.winloss',
-  'player.service',
-  'player.d3'
+  'player.d3',
+  'player.pie'
 ])
   .controller('PlayerCtrl', function($stateParams, getId, solostats, history) {
     var ctrl = this;
@@ -16,25 +17,4 @@ angular.module('player', [
     ctrl.name = $stateParams.name;
     ctrl.solostat = solostats[id][0]['entries'][0];
     ctrl.history = history;
-
-
-    //PlayerModel.getPlayerId(ctrl.name)
-    //  .then(function(id) { // level 1
-    //    //ctrl.id = id;
-    //
-    //    Riot.getLeague(id)
-    //      .then(function(league) {
-    //        store.storeSoloStats(league[id][0]['entries'][0]);
-    //        console.log('hi from main');
-    //        //ctrl.solostats = league[id][0]['entries'][0];
-    //      });
-    //
-    //    Riot.getMatchHistory(id, 10)
-    //      .then(function(history) { // level 2
-    //        store.storeHistory(history.matches);
-    //        ctrl.history = history.matches;
-    //      })
-    //  });
-
-
   });
