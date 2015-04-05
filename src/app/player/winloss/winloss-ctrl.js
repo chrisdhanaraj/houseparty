de058eb64c120b1aa709f1777f1214e4ccd8df60
Chris Dhanaraj <adhanaraj@gmail.com>
2015-04-05 18:31:00 -0400

    setup winloss data and point to directive

--- src/app/player/winloss/winloss-ctrl.js
+++ src/app/player/winloss/winloss-ctrl.js
@@ -3,5 +3,20 @@ angular.module('player.winloss', [])
     var subctrl = this;
     var stats = solostats[getId][0]['entries'][0];
 
-    subctrl.data = stats.wins + ',' + stats.losses;
+    subctrl.data = {
+      labels: ["Wins", "Losses"],
+      datasets: [
+        {
+          label: "Wins",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,0.5)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [stats.wins, stats.losses]
+        }
+      ]
+    };
+
   });
