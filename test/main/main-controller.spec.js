2f17eca69f2a213fb4000cb926adaec34b3ba36c
Chris Dhanaraj <adhanaraj@gmail.com>
2015-04-14 16:42:59 -0400

    main controller test

---
+++ test/main/main-controller.spec.js
@@ -0,0 +1,24 @@
+describe('Main Ctrl:', function() {
+  beforeEach(module('houseparty.common'));
+  beforeEach(module('main.team'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  describe('$scope.remainder', function() {
+    it('should return true when no remainder', function() {
+
+    });
+
+    it('should return false when there is a remainder', function() {
+
+    });
+
+  });
+
+
+
+});
